5b05b8f58700f041cc31dafbdc7c860769e2da8f
Jonathan Graziani <jpgraziani@gmail.com>
2021-06-27 15:54:51 -0700

    init blog flask

---
+++ flaskblog.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+  {
+    'author': 'Cory Bolls',
+    'title': 'Blog Post Food',
+    'content': 'First post content',
+    'date_posted': 'April 20, 2021'
+  },
+  {
+    'author': 'Papa Graziani',
+    'title': 'How to make lamps',
+    'content': 'You need love and glue',
+    'date_posted': 'March 20, 2021'
+  },
+  {
+    'author': 'Olivia Graziani',
+    'title': 'How to not eat lunch',
+    'content': 'Watch cocomelon',
+    'date_posted': 'April 27, 2021'
+  }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+  return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+  return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+  app.run(debug=True)
